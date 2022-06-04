c5f7fc1ccb253e630b87a83b4f35f16473e01d2f
Tin Tran <tinhardware@gmail.com>
2022-06-04 21:31:09 +0700

    Add a horizontal grid layout for the brands

---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Tin Tran on 04/06/2022.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
