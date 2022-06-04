f231c4314343a81a17253b5760448a1707b2156e
Tin Tran <tinhardware@gmail.com>
2022-06-04 15:36:22 +0700

    Update project's structure

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,30 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Tin Tran on 04/06/2022.
+//
+
+import Foundation
+import SwiftUI
+
+// MARK: - DATA
+
+// MARK: - COLOR
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// MARK: - LAYOUT
+
+// MARK: - UX
+
+// MARK: - API
+
+// MARK: - IMAGE
+
+// MARK: - FONT
+
+// MARK: - STRING
+
+// MARK: - MISC
